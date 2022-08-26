fb44e4055321d7d6147644e48693ae1afd05e2a6
yunkim <1106qwe@hanmail.net>
2022-08-26 15:52:59 +0900

    object mapper

--- src/test/java/com/example/response/ResponseApplicationTests.java
+++ src/test/java/com/example/response/ResponseApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.response;
 
+import com.example.response.dto.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +10,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ResponseApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws JsonProcessingException {
+		System.out.println("-----");
+
+		var objectMapper = new ObjectMapper();
+
+		/* object -> text
+		object mapper는 get 메서드를 사용한다.
+		*/
+		var user = new User("yuni", 10, "123-456", "address");
+
+		var text = objectMapper.writeValueAsString(user);
+
+		System.out.println(text);
+
+		/* text -> object
+		기본 생성자가 필요하다. */
+		var objectUser = objectMapper.readValue(text, User.class);
+
+		System.out.println(objectUser);
+
 	}
 
 }
